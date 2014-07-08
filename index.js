be6f7220d9c12982cea0fd93df9aa7f26581fd7e
jbagaresgaray <philipgaray2@gmail.com>
2014-07-08 09:38:47 +0800

    Added new code for unit test.... add mocha on package.json

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var request = require('request');
 var should = require('should');
+var assert = require('assert');
 
 var API_URL = 'http://192.168.3.109:3000/api/data';
+var Sample_API_URL = 'http://192.168.3.109:3000/api/data/a888';
 
 describe('Api Testing', function () {
 	/**
@@ -20,6 +22,19 @@ describe('Api Testing', function () {
 		});
 	});
 
+	describe('GET', function () {
+		it('Should GET the data from the API', function (done){
+			request(Sample_API_URL, function (error, response, body){
+				body = JSON.parse(body);
+				should.not.exist(error);
+				response.statusCode.should.equal(200);
+				body.should.be.instanceOf(Array);
+				body.length.should.be.above(2);
+				done();
+			});
+		});
+	});
+
 	/**
 	 * Saving data to the server
 	 * @return {[type]} [description]
@@ -27,13 +42,13 @@ describe('Api Testing', function () {
 	describe('POST', function () {
 		it('Should SAVE the data to the Server', function (done){
 			var data = {
-				id: '11111',
+				id: '11112',
 				age: 20,
 				price: 30,
 				brandname: 'Cherry Mobile 2',
 				rating: 3,
 				found_id: 'cherry2',
-				name: 'RioCherry2',
+				name: 'RioCherry3',
 				category: 'Phones',
 				subcategory: 'Phones, Tablet',
 				imageUrl: 'NA',
@@ -55,6 +70,41 @@ describe('Api Testing', function () {
 				done();
 			});
 		});
-	});	
+	});
+
+	describe('PUT', function () {
+		it('Should PUT the data from the API', function (done){
+
+			var data = {
+				id: '11112',
+				age: 20,
+				price: 30,
+				brandname: 'Cherry Mobile Flair',
+				rating: 3,
+				found_id: 'cherryFlair',
+				name: 'RioCherry Flair',
+				category: 'Phones',
+				subcategory: 'Phones, Tablet',
+				imageUrl: 'NA',
+				description: 'Nice Phone',
+				snippet: 'sample snippet 2'
+			};
+
+			var options = {
+				url: Sample_API_URL,
+				method: 'PUT',
+				form: data
+			}
+
+			request(options, function (error, response, body){
+				body = JSON.parse(body);
+				should.not.exist(error);
+				response.statusCode.should.equal(200);
+				body.message.should.containEql('Record updated');
+				done();
+			});
+
+		});
+	});
 });
 
